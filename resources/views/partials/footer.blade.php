19a6c09393694327341bd52ad2faf088cc816834
Daniele <danielefontana90@gmail.com>
2020-07-29 17:22:19 +0200

    Header + Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer>
+<div class="container-footer">
+  <div class="left">
+    <ul>
+      <li>Sede operativa</li>
+      <li>Via Carducci 12 - 20123 Milano</li>
+      <li>Tel: 02-40031288</li>
+    </ul>
+  </div>
+  <div class="right">
+    <ul>
+      <li> <a href="#">Lezione gratuita</a> </li>
+      <li> <a href="#">Domande frequenti</a> </li>
+      <li> <a href="#">Privacy</a> </li>
+      <li> <a href="#">Lavora con noi</a> </li>
+    </ul>
+
+  </div>
+
+</div>
+
+</footer>
