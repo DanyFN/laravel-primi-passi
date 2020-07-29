c76827c418d8bfaea7b1a12477e20904f9dcc4d0
Daniele <danielefontana90@gmail.com>
2020-07-29 17:06:24 +0200

    Push #1

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href=" {{ asset("css/app.css") }} ">
+    <title></title>
+  </head>
+  <body>
+    {{-- inludo l'header --}}
+    @include("partials.header")
+    {{-- includo il footer --}}
+    @include("partials.footer")
+
+
+  </body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+  <div class="container">
+    {{-- logo --}}
+    <div class="logo">
+      <img src="img\logo.png" alt="">
+    </div>
+    {{-- navbar --}}
+    <div class="lista">
+
+      <ul>
+        <li>Home</li>
+        <li>Corso</li>
+        <li>Dopo il corso</li>
+        <li>Lezioni a pagamento</li>
+        <li>Assumi i nostri studenti</li>
+      </ul>
+
+    </div>
+    {{-- sezione candidati --}}
+    <div class="candidati">
+      <a href="#"><button type="button" name="button">Candidati Ora</button></a>
+
+    </div>
+
+  </div>
+</header>
